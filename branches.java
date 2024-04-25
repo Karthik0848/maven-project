695bf8bc784d8bc3024cb981844ea1f01291cba3
karthik0848 <dhanushkarthik84@gmail.com>
2024-04-25 11:51:57 +0530

    branches added

---
+++ branches.java
@@ -0,0 +1,27 @@
+package org.add;
+
+public class branches {
+	private void inmakestrivandrum() {
+		System.out.println("trivandrum 123456");
+	}
+	private void inmakeskochi() {
+		System.out.println("kochi987654");
+	}
+	private void inmakeschennai() {
+		System.out.println("chennai456789");
+}
+	private void inmakeshyderbad() {
+		System.out.println("hyderabad");
+	}
+		private void inmakesdubai() {
+			System.out.println("dubai876432");
+		}
+		public static void main(String[]args) {
+			branches n=new branches();
+			n.inmakestrivandrum();
+			n.inmakeskochi();
+			n.inmakeschennai();
+			n.inmakeshyderbad();
+			n.inmakesdubai();
+		}
+}
